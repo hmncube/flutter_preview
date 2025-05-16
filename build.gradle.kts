f09b104246ee121fa026065b07f0c6b6091bee93
happinessncube <happinessmncube@gmail.com>
2025-05-16 15:19:38 +0200

    set pluginSinceBuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,16 +60,6 @@ tasks {
             }
         )
     }
-/*
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }*/
 
     signPlugin {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
