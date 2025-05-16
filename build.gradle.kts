4677b3267e517a0feb08061d8cc0d5333ccfd2c6
happinessncube <happinessmncube@gmail.com>
2025-05-16 10:51:17 +0200

    Update build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,17 @@
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.ChangelogSectionUrlBuilder
+import org.jetbrains.changelog.date
+
+fun properties(key: String) = project.findProperty(key).toString()
+
+fun getFullVersion() = "${properties("appVersion")}-${properties("appPlatformVersion")}"
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.25"
     id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.changelog") version "2.2.1"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
 
 group = "com.hmncube"
@@ -20,6 +30,29 @@ intellij {
     plugins.set(listOf(/* Plugin Dependencies */))
 }
 
+changelog {
+    version.set("1.0.0")
+    path.set(file("CHANGELOG.md").canonicalPath)
+    header.set(provider { "[${version.get()}] - ${date()}" })
+    headerParserRegex.set("""(\d+\.\d+)""".toRegex())
+    introduction.set(
+        """
+        My awesome project that provides a lot of useful features, like:
+        
+        - Feature 1
+        - Feature 2
+        - and Feature 3
+        """.trimIndent()
+    )
+    itemPrefix.set("-")
+    keepUnreleasedSection.set(true)
+    unreleasedTerm.set("[Unreleased]")
+    groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
+    lineSeparator.set("\n")
+    combinePreReleases.set(true)
+    sectionUrlBuilder.set(ChangelogSectionUrlBuilder { repositoryUrl, currentVersion, previousVersion, isUnreleased -> "foo" })
+}
+
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
@@ -29,12 +62,18 @@ tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
     }
-
     patchPluginXml {
-        sinceBuild.set("241")
-        untilBuild.set("243.*")
+        changeNotes.set(provider {
+            changelog.renderItem(
+                changelog
+                    .getUnreleased()
+                    .withHeader(false)
+                    .withEmptySections(false),
+                Changelog.OutputType.HTML
+            )
+        })
     }
-
+/*
     signPlugin {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("PRIVATE_KEY"))
@@ -43,5 +82,25 @@ tasks {
 
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
+    }*/
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        dependsOn("patchPluginXml")
+        token.set(System.getenv("ORG_GRADLE_PROJECT_intellijPublishToken"))
+        channels.set(listOf("default"))
+    }
+
+    register<GradleBuild>("ktlint") {
+        tasks = listOf(
+            "runKtlintFormatOverKotlinScripts",
+            "runKtlintFormatOverMainSourceSet",
+            "runKtlintFormatOverTestSourceSet"
+        )
     }
 }
